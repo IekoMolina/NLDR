21b0b783fda865e983169ac5a8841c27e6f67a98
Unknown <ieko_molina@dlsu.edu.ph>
2017-11-21 08:13:55 +0800

    Add

---
+++ app/QueryModel.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\DB;
+class QueryModel extends Model
+{
+   	public static function getExistingDisaster()
+   	{
+       	$existingData = DB::table('GLIDEEVENT')
+       					->select('GLIDEEVENT.*')
+       					->get();
+       	return $existingData;
+   	}
+
+   	public static function getAllDisaster()
+   	{
+   		$disasterData = DB::table('REF_GLIDEEVENTTYPE')
+   						  ->select('REF_GLIDEEVENTTYPE.DESCRIPTION')
+   						  ->get();
+   		return $disasterData; 
+   	}
+
+   	public static function getAllRegion()
+   	{
+   		$region = DB::table('REF_REGION')
+   					->select('REF_REGION.REGIONCODE')
+   					->get();
+   		return $region;
+   	}
+    /*
+   	//GETS ALL THE INFORMATION ABOUTA DISASER THAN HAPPENED IN A REGION
+   	public static function getReportData()
+   	{
+   		$yearlyData = DB::table('REF_GLIDEEVENTTYPE')
+   						->join('GLIDEEVENT', 'REF_GLIDEEVENTTYPE.GLIDEEVENTTYPECODE','=','GLIDEEVENT.GLIDEEVENTTYPECODE')
+   						->join('PDNA','GLIDEEVENT.GLIDEEVENTID','=','PDNA.GLIDEEVENTID')
+              ->join('PDNADETAILS','PDNA.PDNAID','=','PDNADETAILS.PDNAID')
+              ->join('REF_INCIDENT','PDNA.INCIDENTTYPECODE','=','REF_INCIDENT.INCIDENTTYPECODE')
+              ->join('REF_INCIDENTSOURCE','REF_INCIDENT.INCIDENTSOURCEID','=','REF_INCIDENTSOURCE.INCIDENTSOURCEID') 
+              ->join('RESETTLEMENT','PDNADETAILS.RESETTLEMENTID','=','RESETTLEMENT.RESETTLEMENTID')
+              //recovery
+              ->join('INFRA','PDNADETAILS.INFRAID','=','INFRA.INFRAID')//infra
+              ->join('LIVELIHOOD','PDNADETAILS.LIVELIHOODID','=','LIVELIHOOD.LIVELIHOODID')//livelihhod
+              ->join('REF_LIVELIHOODDMG','LIVELIHOOD.LIVELIHOODDMGID','=','REF_LIVELIHOODDMG.LIVELIHOODDMGID')
+              ->join('SOCIAL','PDNADETAILS.SOCIALID','=','SOCIAL.SOCIALID')//social
+              //subsector Should it be where in?? or join pa din
+              //ref_dmg same here?
+   						->join('REF_CITY/MUNICIPALITY','PDNADETAILS.CITY/MUNICIPALITYID','=','REF_CITY/MUNICIPALITY.CITY/MUNICIPALITYID')
+   						->join('REF_PROVINCE','REF_CITY/MUNICIPALITY.PROVINCEID','=','REF_PROVINCE.PROVINCEID')
+   						->join('REF_REGION','REF_PROVINCE.REGIONCODE','=','REF_REGION.REGIONCODE')
+   						->select('GLIDEEVENT.*','REF_GLIDEEVENTTYPE.*','PDNA.*','REF_CITY/MUNICIPALITY.*','REF_PROVINCE.*','REF_REGION.*','PDNADETAILS.*')
+   						->get();
+   		return $yearlyData;
+   	}
+
+    public static function getReportDataFiltered($req)
+    {
+        $disasterType = $req->input('disasterType');
+        $year = $req->input('year');
+
+            $yearlyData = DB::table('REF_GLIDEEVENTTYPE')
+              ->join('GLIDEEVENT', 'REF_GLIDEEVENTTYPE.GLIDEEVENTTYPECODE','=','GLIDEEVENT.GLIDEEVENTTYPECODE')
+              ->join('PDNA','GLIDEEVENT.GLIDEEVENTID','=','PDNA.GLIDEEVENTID')
+              ->join('PDNADETAILS','PDNA.PDNAID','=','PDNADETAILS.PDNAID')
+              ->join('REF_INCIDENT','PDNA.INCIDENTTYPECODE','=','REF_INCIDENT.INCIDENTTYPECODE')
+              ->join('REF_INCIDENTSOURCE','REF_INCIDENT.INCIDENTSOURCEID','=','REF_INCIDENTSOURCE.INCIDENTSOURCEID') 
+              ->join('RESETTLEMENT','PDNADETAILS.RESETTLEMENTID','=','RESETTLEMENT.RESETTLEMENTID')
+              //recovery
+              ->join('INFRA','PDNADETAILS.INFRAID','=','INFRA.INFRAID')//infra
+              ->join('LIVELIHOOD','PDNADETAILS.LIVELIHOODID','=','LIVELIHOOD.LIVELIHOODID')//livelihhod
+              ->join('REF_LIVELIHOODDMG','LIVELIHOOD.LIVELIHOODDMGID','=','REF_LIVELIHOODDMG.LIVELIHOODDMGID')
+              ->join('SOCIAL','PDNADETAILS.SOCIALID','=','SOCIAL.SOCIALID')//social
+              //subsector Should it be where in?? or join pa din
+              //ref_dmg same here?
+              ->join('REF_CITY/MUNICIPALITY','PDNADETAILS.CITY/MUNICIPALITYID','=','REF_CITY/MUNICIPALITY.CITY/MUNICIPALITYID')
+              ->join('REF_PROVINCE','REF_CITY/MUNICIPALITY.PROVINCEID','=','REF_PROVINCE.PROVINCEID')
+              ->join('REF_REGION','REF_PROVINCE.REGIONCODE','=','REF_REGION.REGIONCODE')
+              ->select('GLIDEEVENT.*','REF_GLIDEEVENTTYPE.*','PDNA.*','REF_CITY/MUNICIPALITY.*','REF_PROVINCE.*','REF_REGION.*','PDNADETAILS.*')
+              ->where([
+                          ['REF_GLIDEEVENTTYPE.DESCRIPTION', '=', $disasterType],
+                          ['GLIDEEVENT.STARTDATE', '=', $year]
+                      ])
+              ->get();
+      return $yearlyData;
+    }
+    */
+    /*
+    public static function getPDNA()
+    {
+      $pdnaData = DB::table('')
+    }
+
+    public static function getSector()
+    {
+      $sectorData = DB::table('')
+    }
+
+    public static function getLocation()
+    {
+      $pdnaData = DB::table('')
+    }
+    */
+}
---
+++ resources/views/encodingNewDisasterCity.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>National Loss and Damage Registry</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet"> 
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin: 0px 0px 0px 0px;">
+            <div class="navbar-header">
+                     <a class="navbar-brand" href="home">
+                        <img src="../img/RNP.png" style="width:30px;height:30px; margin: 0px 0px 0px 0px;"> 
+                     </a>        
+                    <a class="navbar-brand" href="home">        <font color="white">Home    </font></a>
+                    <a class="navbar-brand" href="reports">     <font color="white">Report  </font></a>
+                    <a class="navbar-brand" href="encoding">  <font color="white">Encode  </font></a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: white;">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> Administrator</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Roles</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="login"><i class="fa fa-sign-out fa-fw"></i> Login</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+        </nav>
+
+            <!-- Banner row -->
+            <div class="row">
+                <div class="col-lg-12" style="background-color: rgba(100,149,237,0.6);" >               
+                 <h2 align="left">                   
+                    <font> Encode Disaster Information </font>
+                 </h2>
+                    <!--<font style="font-size: 20px; color: white;">  National Disaster Risk Reduction and Management Council </font>  -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                        </div>
+                    </div>                 
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-primary">
+                        <!-- Panel Body -->
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <!--
+                                    <form action="/generateReport" method="post">
+                                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                        <div class="form-group col-lg-6">
+                                            <label>Disaster Type</label>
+                                            <select class="form-control" name="disasterType">
+                                                <option>TYPHOON</option>
+                                                <option>EARTHQUAKE</option>
+                                                <option>FLASH FLOOD</option>
+                                                <option>FIRE</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-lg-6">
+                                            <label>Year</label>
+                                            <select class="form-control" name="year">
+                                                <option>2013</option>
+                                                <option>2014</option>
+                                                <option>2015</option>
+                                                <option>2016</option>
+                                            </select>
+                                        </div>
+
+                                        <div align="text-center" class="form-group col-lg-3" >
+                                            <input type="submit" name="submit" value="submit">
+                                        </div>
+                                    </form>
+                                    -->
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="panel panel-default">
+                                                <div class="panel-heading">
+                                                    Input New Disaster
+                                                </div>
+                                                <div class="panel-body">
+                                                    <div class="row">
+                                                        <div class="col-lg-6">
+                                                            <form role="form" method="post"> <!--action="/encodingNewDisaster"-->
+                                                                <div class="form-group">
+                                                                    <label>Select Provinces Involved</label>
+                                                                        <select multiple class="form-control" name="provincesAffected">
+                                                                            <option>Cavite</option>
+                                                                            <option>Laguna</option>
+                                                                            <option>Batangas</option>
+                                                                            <option>Rizal</option>
+                                                                            <option>Quezon Province</option>
+                                                                        </select>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                <a type="submit" name="submit" class="btn btn-default" href='/encodingNewDisasterDetails'>Next</a>
+                                                                <button type="reset" name="reset" class="btn btn-default">Reset</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.col-lg-8 (nested) -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-8 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+</html>
